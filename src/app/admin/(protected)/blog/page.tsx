df6129a2b95cfbd30d4c4f012133851261107686
Wajid Ali Arya <wajid.arya2012@gmail.com>
2026-07-04 12:31:32 +0430

    Save Button Deleted

--- src/app/admin/(protected)/blog/page.tsx
+++ src/app/admin/(protected)/blog/page.tsx
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
-import DeletePostBtn from "./DeletePostBtn";
 export const metadata = { title: "Blog | Admin" };
 export default async function BlogPage() {
   const posts = await prisma.blogPost.findMany({ orderBy:{ createdAt:"desc" } });
